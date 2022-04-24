222b318187eb1ec9986777eb73a1ce498598aed2
Perestoronin Pavel <p_perestoronin@mail.ru>
2022-04-24 19:01:41 +0300

    [GH-37] Merge logging to mvp

--- bongocore/server/src/HandlerFactory.cpp
+++ bongocore/server/src/HandlerFactory.cpp
@@ -4,26 +4,31 @@
 #include <sstream>
 #include "clients/HttpPrimitives.h"
 
-RequestHandler::RequestHandler(bongodb::Backend::TBackend* backend) {
-    Backend = backend;
+RequestHandler::RequestHandler(Poco::Logger& logger, bongodb::Backend::TBackend& backend)
+    : Backend(backend)
+    , Logger(logger)
+{
 }
 
 void RequestHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
 {
-    // Poco::Util::Application& app = Poco::Util::Application::instance();
-    // app.logger().debug("Request from %s", request.clientAddress().toString());
     Poco::Net::HTTPRequest pocoRequest = Poco::Net::HTTPRequest(request.getMethod(), request.getURI());
     std::ostringstream ss;
     Poco::StreamCopier::copyStream(request.stream(), ss);
 
     auto httpRequest = bongodb::Clients::THttpRequest::FromPocoHttpRequest(pocoRequest, ss.str());
-    auto backendResponse = Backend->Process(std::move(httpRequest));
+    Logger.debug("Request from %s", request.clientAddress().toString());
+    try {
+        auto backendResponse = Backend.Process(std::move(httpRequest));
 
-    response.setChunkedTransferEncoding(true);
-    response.setContentType("text/json");
-    response.setStatus(backendResponse.GetStatus());
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("text/json");
+        response.setStatus(backendResponse.GetStatus());
 
-    response.send() << backendResponse.GetBody();
+        response.send() << backendResponse.GetBody();
+    } catch (...) {
+        Logger.debug("Not processed(((");
+    }
 }
 
 RequestHandlerFactory::RequestHandlerFactory(const Poco::Util::AbstractConfiguration& config) {
@@ -32,5 +37,5 @@ RequestHandlerFactory::RequestHandlerFactory(const Poco::Util::AbstractConfigura
 
 Poco::Net::HTTPRequestHandler* RequestHandlerFactory::createRequestHandler(const Poco::Net::HTTPServerRequest&)
 {
-    return new RequestHandler(Backend.get());
+    return new RequestHandler(Logger, *Backend);
 }
